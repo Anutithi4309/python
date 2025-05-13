5536fc4eb06ceaf23d69bd65674769b4fbc85dec
Anutithi4309 <elorasumi1977@gmail.com>
2025-05-13 22:38:52 +0600

    Add files via upload

---
+++ oparaters in python.py
@@ -0,0 +1,16 @@
+
+first_name="Anutithi"
+last_name="paul"
+full_name=first_name+last_name
+example="Haa*5"
+print("First_name:",first_name)
+print("last_name:",last_name)
+print("full_name:",full_name)
+print("Strings Multiplied 5 times gives this result:",example)
+word='Anu'
+print("Lenth of String:",len(word))
+print("First Letter of String:",word[0])
+print("Last Letter of String:",word[2])
+print("String Sliced",word[0:3])
+
+
